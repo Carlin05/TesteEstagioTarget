51e20410abd33a3fe85876a1e4e61a6ec5287771
GUTO <GUTO@DESKTOP-OA38TJF>
2024-08-30 10:45:09 -0300

    Primeiro Comit

---
+++ EstagioTarget/src/controller/Questoes.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class Questoes {
+	public Questoes() {
+		super();
+	}
+	public void questao1() {
+		int indice = 13, soma = 0, k = 0;
+		while(k < indice) {
+			k++;
+			soma += k;
+		}
+		System.out.println(soma);
+	}
+	private  
+}
